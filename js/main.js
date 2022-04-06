f8a686e8acfaa28d063735bc3c2778a464d0d47f
khalilnazari <khalil.deve@gmail.com>
2022-04-07 00:04:33 +0800

    View email page is developed

--- js/main.js
+++ js/main.js
@@ -40,3 +40,59 @@ navbar_btn.addEventListener('click', () => {
     sidenav.classList.toggle('small-sidenav') 
 })
 
+
+
+const select_colapse_email = document.querySelectorAll('.select-colapse-email')
+const action_btns = document.querySelector('.action-btns')
+const select_all_colapse_email = document.getElementById('select-all-colapse-email')
+
+// selected all collapsed email
+select_all_colapse_email.addEventListener('change', ele => {
+    if(ele.target.checked) {
+        select_colapse_email.forEach(checkbox => {
+            checkbox.checked = true; 
+
+        }); 
+    } else {
+        select_colapse_email.forEach(checkbox => {
+            checkbox.checked = false 
+        }); 
+    }
+})
+
+// check even if any colapsed email is selected
+select_colapse_email.forEach(input => {
+    input.addEventListener('change', e => {
+        const ifchecked = ifChecked()
+        if(ifchecked) {
+            action_btns.classList.remove('text-secondary')
+        } else {
+            action_btns.classList.add('text-secondary')
+        }
+    })
+})
+
+// check if any of colapsed email is selected
+function ifChecked() {
+    const restul = [...select_colapse_email].filter(ele => ele.checked)
+    if(restul.length != 0) {
+        return true
+    }
+    else {
+        return false;
+    }
+}
+
+
+// change email listing title 
+// const email_labels = document.querySelectorAll('.email-label'); 
+// email_labels.forEach(ele => {
+//     ele.addEventListener('click', (e) => {
+//         const icon = e.target.querySelector('i'); 
+//         const iconClasses = [...icon.classList].join(" ");
+
+//         let spantext = e.target.querySelector('span'); 
+//         spantext = spantext.innerText; 
+//         console.log(spantext)
+//     })
+// })
